bb4976b98f23ab4efefc09d167b72a2f23f6cf29
hector ladero <hectorladero@gmail.com>
2021-04-12 18:14:05 -0400

    upload code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,9 @@
-d3.json("samples.json").then((samples) => {
-    //var otu_ids = samples.samples[1];
+function buildplot(selection){
+    d3.json("samples.json").then((samples) => { 
+    //var samples = samples.sample_values.slide(0,10);
+    var otu_ids = samples[1];
+    
+    console.log(samples.otu_ids);
     var trace1= {
         x: samples.samples.otu_ids,
         y: samples.samples.samples_values,
@@ -12,34 +16,7 @@ d3.json("samples.json").then((samples) => {
     };
 
     Plotly.newPlot("bar",data,layout);
-});
-
-function BubChart() {
-    d3.json("samples.json").then(({bublechart})=>{
-
-        var xvalues = bublechart.otu_ids;
-        var yvalues = bublechart.sample_values;
-        var markersize = bublechart.sample_values;
-        var markercolors= bublechart.otu_ids;
-
-        var traceA= {
-            x: xvalues,
-            y: yvalues,
-            mode: 'markers',
-            marker:{
-                size: markersize,
-                color: markercolors
-            }
-        };
-
-        var data = [traceA];
-        var layout={
-            title: "otu_ids"
-        };
-
-        Plotly.newPlot("bubble",data,layout)
 })};
-
 
 d3.json('samples.json').then(({names})=>{
     names.forEach(name => {
@@ -56,9 +33,45 @@ function show() {
     d3.json("samples.json").then(({metadata,samples})=>{
         var selection = d3.select("#selDataset").node().value;
         var meta= metadata.filter(obj=>obj.id == selection)[0];
+        var sample= samples.filter(obj=>obj.id == selection)[0];
+
         Object.entries(meta).forEach(([key,val])=> {
         d3.select('#sample-metadata').append('h5').text(key.toUpperCase()+': '+val)    
         }) 
-        console.log(meta);
+
+            var xvalues = sample.otu_ids;
+            var yvalues = sample.sample_values;
+            var markersize = sample.sample_values;
+            var markercolors= sample.otu_ids;
+        
+            var traceA= [{
+                x: xvalues,
+                y: yvalues,
+                mode: 'markers',
+                marker:{
+                    size: markersize,
+                    color: markercolors
+                }
+            }]
+            var data = [traceA];
+            var layout={
+                title: "otu_ids"
+            };
+            Plotly.newPlot("bubble",data,layout);
+
+
+        console.log(sample);
     })
 };
+
+
+// d3.json("samples.json").then(({sample})=>{
+
+// });
+// function Handlesubmit() {
+//     d3.event.preventdefault();
+//     var id= d3.select('#selDataset').node().value
+//     console.log(id);
+//     buildplot(selection);
+
+// }
